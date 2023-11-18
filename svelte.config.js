cf6ac3844f622e07eeab86f0d798a267e25e4ca6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-11-18 13:27:22 +0700

    build: add deploy script

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,10 @@ const config = {
   })],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/duydang2311.github.io' : '',
+    }
 	},
 };
 

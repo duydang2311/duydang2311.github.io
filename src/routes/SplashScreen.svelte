3798ada1cff65da0485146c59f4a162d63b366cd
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-11-21 01:47:10 +0700

    chore: remove unused fonts

--- src/routes/SplashScreen.svelte
+++ src/routes/SplashScreen.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { stagger, timeline } from 'motion';
-	import { eachHourOfInterval } from 'date-fns';
 
 	let div: HTMLDivElement;
-	let h1: HTMLElement;
 	onMount(() => {
 		timeline([
 			[
@@ -17,14 +15,18 @@
 				{ opacity: 0, transform: 'translateY(0.5rem)' },
 				{ at: 1, delay: stagger(0.04), duration: 0.2, easing: 'ease-in' }
 			],
-			[div, { opacity: 0 }, { at: 1.25, duration: 0.5, easing: 'ease-in-out' }],
+			[
+				div,
+				{ opacity: 0, pointerEvents: 'none' },
+				{ at: 1.25, duration: 0.5, easing: 'ease-in-out' }
+			],
 			[div, { display: 'none' }]
 		]);
 	});
 </script>
 
 <div bind:this={div} class="fixed flex justify-center items-center inset-0 bg-white">
-	<h1 bind:this={h1} class="font-black text-7xl">
+	<h1 class="font-black text-7xl">
 		{#each 'duydang' as letter}
 			<span class="letter opacity-0 translate-y-2 inline-block">{letter}</span>
 		{/each}
